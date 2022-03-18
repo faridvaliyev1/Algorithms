3a1c7d3ac76f84ae4b08ef8ef1bc226a9ab34f78
faridval <faridvaliyevgit@gmail.com>
2022-03-19 01:48:11 +0200

    Traversing linked list

--- Data Structures/LinkedList.py
+++ Data Structures/LinkedList.py
@@ -1,6 +1,12 @@
 class LinkedList:
-    def __init__(self):
+    def __init__(self,nodes=None):
         self.head =None
+        if nodes is not None:
+            node=Node(data=nodes.pop(0))
+            self.head=node
+            for elem in nodes:
+                node.next=Node(data=elem)
+                node=node.next
     
     def __repr__(self):
         node=self.head
@@ -12,7 +18,12 @@ class LinkedList:
         nodes.append("None")
         return " -> ".join(nodes)
 
-
+    def __iter__(self):
+        node=self.head
+        while node is not None:
+            yield node
+            node=node.next
+    
 class Node:
     def __init__(self,data):
         self.data=data
