41cfed31b66fa43f4caff236ced6c0da20fead77
faridval <faridvaliyevgit@gmail.com>
2022-03-19 01:51:16 +0200

    Adding first and last node

--- Data Structures/LinkedList.py
+++ Data Structures/LinkedList.py
@@ -24,6 +24,20 @@ class LinkedList:
             yield node
             node=node.next
     
+    def add_first(self,node):
+        node.next=self.head
+        self.head=node
+    
+    def add_last(self,node):
+        if self.head is None:
+            self.head=node
+            return
+        
+        for current_node in self:
+            pass
+        
+        current_node.next=node
+        
 class Node:
     def __init__(self,data):
         self.data=data
